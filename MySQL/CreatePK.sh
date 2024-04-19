390ba0d7e223df15133d57bc49a0228d47a414d1
Sequeira12 <brunomsequeira@hotmail.com>
2024-04-19 18:10:14 +0100

    inicio do report

--- MySQL/CreatePK.sh
+++ MySQL/CreatePK.sh
@@ -4,51 +4,51 @@
 # Executa a consulta e armazena o resultado na variável 'output'
 rm -r Results/PKCreation.txt
 
-output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud; ALTER TABLE REGION ADD PRIMARY KEY (R_REGIONKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
+output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud;SET profiling = 1; ALTER TABLE REGION ADD PRIMARY KEY (R_REGIONKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
 
 echo "Time create PK R_REGIONKEY into Table REGION " $output >> Results/PKCreation.txt
 
 
 
 
-output=$(mysql -u root -h localhost -P 3306 -e "USE tpchcloud; ALTER TABLE NATION ADD PRIMARY KEY (N_NATIONKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
+output=$(mysql -u root -h localhost -P 3306 -e "USE tpchcloud;SET profiling = 1; ALTER TABLE NATION ADD PRIMARY KEY (N_NATIONKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
 
 echo "Time create PK N_NATIONKEY into Table NATION " $output >> Results/PKCreation.txt
 
 
 
 
-output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud; ALTER TABLE CUSTOMER ADD PRIMARY KEY (C_CUSTKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
+output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud;SET profiling = 1; ALTER TABLE CUSTOMER ADD PRIMARY KEY (C_CUSTKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
 
 echo "Time create PK C_CUSTKEY into Table CUSTOMER " $output >> Results/PKCreation.txt
 
 
 
 
-output=$(mysql -u root -h localhost -P 3306 -e "USE tpchcloud; ALTER TABLE SUPPLIER ADD PRIMARY KEY (S_SUPPKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
+output=$(mysql -u root -h localhost -P 3306 -e "USE tpchcloud;SET profiling = 1; ALTER TABLE SUPPLIER ADD PRIMARY KEY (S_SUPPKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
 
 echo "Time create PK S_SUPPKEY into Table SUPPLIER " $output >> Results/PKCreation.txt
 
 
 
-output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud; ALTER TABLE PART ADD PRIMARY KEY (P_PARTKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
+output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud;SET profiling = 1; ALTER TABLE PART ADD PRIMARY KEY (P_PARTKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
 
 echo "Time create PK P_PARTKEY into Table PART " $output >> Results/PKCreation.txt
 
 
 
-output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud; ALTER TABLE PARTSUPP ADD PRIMARY KEY (PS_PARTKEY, PS_SUPPKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
+output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud;SET profiling = 1; ALTER TABLE PARTSUPP ADD PRIMARY KEY (PS_PARTKEY, PS_SUPPKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
 
 echo "Time create PK PS_PARTKEY,PS_SUPPKEY into Table PARTSUPP " $output >> Results/PKCreation.txt
 
 
 
-output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud; ALTER TABLE ORDERS ADD PRIMARY KEY (O_ORDERKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
+output=$(mysql -u root -h localhost -P 3306  -e "USE tpchcloud;SET profiling = 1; ALTER TABLE ORDERS ADD PRIMARY KEY (O_ORDERKEY);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
 
 echo "Time create PK O_ORDERKEY into Table ORDERS " $output >> Results/PKCreation.txt
 
 
 
-output=$(mysql -u root -h localhost -P 3306 -e "USE tpchcloud; ALTER TABLE LINEITEM ADD PRIMARY KEY (L_ORDERKEY, L_LINENUMBER);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
+output=$(mysql -u root -h localhost -P 3306 -e "USE tpchcloud;SET profiling = 1; ALTER TABLE LINEITEM ADD PRIMARY KEY (L_ORDERKEY, L_LINENUMBER);SHOW PROFILES" |  grep -A 1 "Duration" |tail -n 1 |awk '{print $2}')
 
 echo "Time create PK L_ORDERKEY, L_LINENUMBER into Table LINEITEM " $output >> Results/PKCreation.txt
